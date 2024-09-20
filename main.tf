cc59c357436f70d831b3d5090ac6fac15b1848f5
Mohan Edala <mohan198631@gmail.com>
2024-09-20 14:50:33 -0500

    IAC-0000 add tf lint and syntax check workflows

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-# How can we use terraform to dynamically deploy a VPC with 1 subnet in each availability zone.
-# •Region value and VPC CIDR block will be given as input.
-# •Terraform should dynamically provision a subnet for each availability Zone.
-
-
-# •Example 1 – N California – us-west-1 – 2 availability zonesVPC CIDR – 10.0.0.0/16Subnet CIDRs – [ 10.0.0.0/17, 10.0.128.0/17 ]
-
-# •Example 2 – Oregon – us-west-2 – 4 availability zonesVPC CIDR – 10.0.0.0/16Subnet CIDRs – [ 10.0.0.0/18, 10.0.64.0/18, 10.0.128.0/18, 10.0.192.0/18 ]
-
-
 # Create a VPC
 data "aws_availability_zones" "available" {}
 
